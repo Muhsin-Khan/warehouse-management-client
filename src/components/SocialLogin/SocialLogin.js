33f25c4e1cbb9db66b4733baf1133553aa64f121
Muhsin Khan <mdmuhsinuddin01@gmail.com>
2022-05-14 07:56:25 +0600

    solving api invalid problem

--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./SocialLogin.css";
 import google from "../../Social icon/google.png";
-import fb from "../../Social icon/fb.png";
-import git from "../../Social icon/github.png";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
   const navigate = useNavigate();
   let errorElement;
   if (error) {
-    errorElement = <div>
+    errorElement = (
+      <div>
         <p className="text-danger">Error: {error.message}</p>
       </div>
-    
+    );
   }
   if (user) {
     navigate("/home");
   }
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
+  //   if (loading) {
+  //     return <p>Loading...</p>;
+  //   }
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -32,14 +31,13 @@ const SocialLogin = () => {
       </div>
       {errorElement}
       <div>
-        <button 
-            onClick={()=>signInWithGoogle()}
-            className="btn btn-info w-50 d-block mx-auto my-2">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-info w-50 d-block mx-auto my-2"
+        >
           <img style={{ width: "30px" }} src={google} alt="" />
           <span className="px-2">Google Sign In</span>
         </button>
-        
-        
       </div>
     </div>
   );
